5dee24119f8c89204c81c1fc6a7e6b893868cd1f
finn-chan <life4aran@outlook.com>
2024-01-28 19:39:14 +0800

    2023 年舊校園網登錄系統

---
+++ .old/Windows/main.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import auth
+import json
+import time
+import send_to
+import win11toast
+import match_network
+
+# 讀取配置文件
+with open('config.json', 'r') as config_file:
+    config_data = json.load(config_file)
+    # print(config_data['userid'])
+
+cycles = 0
+while True:
+    state = match_network.match_cqjtu(config_data['school_url'], config_data['internet_url'], config_data['timeout'])
+    if state == 1:
+        if auth.DoLogOut(config_data['log_out_page'], config_data['timeout']):  # 登入前先登出
+            time.sleep(config_data['timeout'])
+            if auth.DoLogIN(config_data['userid'], config_data['password'], config_data['log_in_page'],
+                            config_data['timeout']):  # 登入
+                time.sleep(2)
+                if config_data['ifttt']:  # 發送訊息給 IFTTT
+                    time.sleep(2)
+                    send_to.ifttt(config_data['webhooks_name'], config_data['webhooks_key'], config_data['timeout'])
+                win11toast.toast('重慶交通大學', '已連接校園網',
+                                 icon=os.path.dirname(os.path.abspath(sys.argv[0])) + '\\cqjtu.ico')
+
+    elif state == 2:  # 已連接校園網
+        if cycles:
+            pass
+        else:  # 初次彈窗提醒
+            win11toast.toast('重慶交通大學', '已連接校園網',
+                             icon=os.path.dirname(os.path.abspath(sys.argv[0])) + '\\cqjtu.ico')
+
+    elif state == 0:  # 未連接校園網，程序終止
+        # print('Over!')
+        break
+
+    cycles += 1
+    time.sleep(30)
+
+# auth.DoLogOut(config_data['log_out_page'],config_data['timeout'])
+# send_to.ifttt(config_data['webhooks_name'], config_data['webhooks_key'], config_data['timeout'])
---
+++ .old/Windows/match_network.py
@@ -0,0 +1,30 @@
+import psutil
+import requests
+import datetime
+
+
+def match_cqjtu(school_url, internet_url, timeout):
+    try:
+        response = requests.get(school_url, timeout=timeout)  # 檢查是否鏈接校園網
+
+        if response.status_code == 200:  # 狀態碼200為連接校園網
+            if datetime.time(6, 30) <= datetime.datetime.now().time() <= datetime.time(23, 29) or (
+                    datetime.time(23, 30) <= datetime.datetime.now().time() <= datetime.time(6,
+                                                                                             29) and psutil.sensors_battery().power_plugged):  # 判斷校園網是否供網
+                try:
+                    response = requests.get(internet_url, timeout=timeout)  # 檢查校園網是否登錄
+                    if response.status_code == 204:  # 狀態碼204為登入校園網
+                        return 2  # 已連接互聯網
+                    else:
+                        return 1  # 未連接互聯網
+
+                except requests.exceptions.RequestException:
+                    return 1  # 未連接互聯網
+            else:
+                return 2  # 已連接校園網，但不在供網時間内
+
+        else:
+            return 0  # 未連接校園網
+
+    except requests.exceptions.RequestException:
+        return 0  # 未連接校園網
